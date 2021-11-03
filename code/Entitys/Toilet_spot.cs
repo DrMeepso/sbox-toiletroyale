9c5850fad45d14f1dc825c9f0620b8a439063690
Whats the time? <50252724+DrMeepso@users.noreply.github.com>
2021-11-03 13:22:29 +1300

    Update Toilet_spot.cs

--- code/Entitys/Toilet_spot.cs
+++ code/Entitys/Toilet_spot.cs
@@ -1,10 +1,10 @@
-﻿using Sandbox;
+using Sandbox;
 using System.Linq;
 using System;
 
 [Library( "toilet_spot", Description = "Spawn a player here" )]
 [Hammer.EditorModel( "models/toilet.vmdl" )]
-[Hammer.EntityTool( "Player Spawnpoint", "Death", "Defines a point where a player can spawn" )]
+[Hammer.EntityTool( "Player Shitpoint", "Toilet", "Defines a point where a player can spawn" )]
 public partial class toiletspot : Prop
 {
 	public Entity Claimer;
