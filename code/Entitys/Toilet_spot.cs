c144a7420d984aa257fc379c39dda7583aa7156e
Whats the time? <50252724+DrMeepso@users.noreply.github.com>
2021-11-03 13:22:56 +1300

    Update Toilet_spot.cs

--- code/Entitys/Toilet_spot.cs
+++ code/Entitys/Toilet_spot.cs
@@ -4,13 +4,12 @@ using System;
 
 [Library( "toilet_spot", Description = "Spawn a player here" )]
 [Hammer.EditorModel( "models/toilet.vmdl" )]
-[Hammer.EntityTool( "Player Shitpoint", "Toilet", "Defines a point where a player can spawn" )]
+[Hammer.EntityTool( "Player Shitpoint", "Toilet", "Defines a point where a player can shit" )]
 public partial class toiletspot : Prop
 {
 	public Entity Claimer;
 	public override void Spawn()
 	{
-		Log.Info( "Spawned Toilet" );
 		SetModel( "models/toilet.vmdl" );
 		base.Spawn();
 	}
